aaa6dc70add150f6fe60ad2c86a84e3dcd41a09a
GabrielleHoyte <gabriellehoyte@gmail.com>
2022-05-30 20:26:52 -0400

    Initial commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = { //exporting an object
+
+    'url' : 'mongodb+srv://GabrielleHoyte:Password0@cluster0.ioxl7.mongodb.net/pigLatin?retryWrites=true&w=majority', 
+    'dbName': 'messages'
+};
